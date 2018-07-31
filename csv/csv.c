07f329f87a099089c723e5a35ac80e095393b9fd
frost <kharchilindanila@gmail.com>
2018-08-01 02:34:53 +0400

    ADD:struct +csv

--- csv/csv.c
+++ csv/csv.c
@@ -1,10 +1,30 @@
 #include <stdio.h>
+#include <string.h>
+
+struct Person {
+	char last[20];
+	char first[20];
+	char middle[20];
+	int  day;
+	char month[10];
+	int  year;
+};
+
+void printperson(struct Person p) {
+	printf("\nPerson:\n");
+	printf("    last: %s\n", p.last);
+	printf("    first: %s\n", p.first);
+	printf("    middle: %s\n", p.middle);
+	printf("    day: %i\n", p.day);
+	printf("    month: %s\n", p.month);
+	printf("    year: %i\n", p.year);
+}
 
 int main() {
-	char line[80];
-	printf("Введите строку csv:\n");
-	scanf("%80s[^\n]", line);
-	printf("Вы ввели строку:\n");
-	printf("%s\n", line);
+	char line[] = "Петров,Сидор,Петрович,20,мая,1978";
+	struct Person p;
+	char *format = "%[^,],%[^,],%[^,],%i,%[^,],%i";
+	sscanf(line, format, p.last, p.first, p.middle, &p.day, p.month, &p.year);
+	printperson(p);
 	return 0;
 }
